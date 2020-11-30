3a752f87bc56663bbf6ca0c2185e00bf08d39133
ergl1s <bibikov2000@mail.ru>
2020-12-01 01:04:42 +0300

    feat: add solution to the first task

--- RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
+++ RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
@@ -4,7 +4,18 @@
 
 // Complete the following fuction
 - (NSArray *)convertToHappy:(NSArray *)sadArray {
-    return @[];
+    
+    if ([sadArray isEqual:@[]]) return sadArray;
+    NSMutableArray* happyArray = [NSMutableArray arrayWithArray:sadArray];
+    
+    for (int i = 1; i < [happyArray count] - 1; i++) {
+        if ([happyArray[i] intValue] > [happyArray[i - 1] intValue] +
+            [happyArray[i + 1] intValue]) {
+            [happyArray removeObjectAtIndex:i];
+            i = 1;
+        }
+    }
+    return happyArray;
 }
 
 @end
