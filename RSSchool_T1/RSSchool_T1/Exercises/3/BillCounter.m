4f1b0334de1c0cb64c10b995938579831885ee66
ergl1s <bibikov2000@mail.ru>
2020-12-04 00:39:45 +0300

    feat: add solution for 2nd and 3rd tasks

--- RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
+++ RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
@@ -4,7 +4,27 @@
 
 // Complete the following fuction
 - (NSString*)compareResultForBill:(NSArray<NSNumber*>*)bill notIncludingElementWithIndex:(NSInteger)index withGivenSum:(NSNumber*)sum {
-    return @"";
+    
+    int sumOfItemsWithoutAnna = 0;
+   
+    int counter = 0;
+    for (NSNumber* item in bill) {
+        if (counter != index) {
+            sumOfItemsWithoutAnna += [item intValue];
+        };
+        counter++;
+    }
+    
+    int result = 0;
+    int eachHasToPay = sumOfItemsWithoutAnna / 2;
+    if (eachHasToPay < [sum intValue]) {
+        result = [sum intValue] - eachHasToPay;
+    }
+    
+    if (result == 0)
+        return @"Bon Appetit";
+    else
+        return [NSString stringWithFormat:@"%i", result];
 }
 
 @end
