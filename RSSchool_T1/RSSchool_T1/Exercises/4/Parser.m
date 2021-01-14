14ccae6ca9bc53eeec0ae50ab413bb4015ec4bab
ergl1s <bibikov2000@mail.ru>
2021-01-14 14:57:46 +0300

    feat: add solution for 4th task

--- RSSchool_T1/RSSchool_T1/Exercises/4/Parser.m
+++ RSSchool_T1/RSSchool_T1/Exercises/4/Parser.m
@@ -4,7 +4,33 @@
 
 // Complete the parseString function below.
 - (NSArray <NSString*>*)parseString:(NSString*)string {
-    return @[@"Test"];
-}
+  NSArray* openBrackets = @[@"(", @"<", @"["];
+  NSArray* closeBrackets = @[@")", @">", @"]"];
+  
+  NSMutableArray<NSString*>* result = [NSMutableArray arrayWithCapacity:10];
+  int repeat = 0; //counter of repeats of the same open brackets
 
+  for (int i = 0; i < [string length]; i++) {
+    for(int j = 0; j < 3; j++) {
+      if ([NSString stringWithFormat:@"%C", [string characterAtIndex:i]] == [NSString stringWithFormat:@"%@", [openBrackets objectAtIndex:j]]) {
+        
+        for (int z = i + 1; z < [string length]; z++) {
+          if ([NSString stringWithFormat:@"%C", [string characterAtIndex:z]] == [NSString stringWithFormat:@"%@", [openBrackets objectAtIndex:j]]) {
+            repeat++;
+            continue;
+          }
+          if ([NSString stringWithFormat:@"%C", [string characterAtIndex:z]] == [NSString stringWithFormat:@"%@", [closeBrackets objectAtIndex:j]] && repeat != 0) {
+            repeat--;
+            continue;
+          }
+          if ([NSString stringWithFormat:@"%C", [string characterAtIndex:z]] == [NSString stringWithFormat:@"%@", [closeBrackets objectAtIndex:j]] && repeat == 0) {
+            [result addObject:[string substringWithRange:NSMakeRange(i + 1, z - i - 1)]];
+            break;
+          }
+        }
+      }
+    }
+  }
+  return result;
+}
 @end
